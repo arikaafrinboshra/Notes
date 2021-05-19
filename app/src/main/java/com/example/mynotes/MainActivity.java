68179896f5300242012cf82133f28c94b0d4a934
Arika Afrin Boshra <arika@qcoom.com>
2021-05-19 18:29:15 +0600

    no message

---
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.mynotes;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.listOfNotes);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add) {
+            Intent i = new Intent(this,AddNote.class);
+            startActivity(i);
+            //Toast.makeText(this, "Add button is clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/mynotes/Note.java
@@ -0,0 +1,45 @@
+package com.example.mynotes;
+
+public class Note {
+
+     private long id;
+     private String title;
+     private String content;
+     private String date;
+     private String time;
+
+     Note() {}
+     Note(String title, String content, String date, String time) {
+         this.title = title;
+         this.content = content;
+         this.date = date;
+         this.time = time;
+     }
+     Note(long id, String title, String content, String date, String time) {
+         this.id = id;
+         this.title = title;
+         this.content = content;
+         this.date = date;
+         this.time = time;
+     }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
